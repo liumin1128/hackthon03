67c75a363d0ac55f6c42605d1cb40d0bc40f6da3
liumin <min_liu@singaporeair.com.sg>
2022-04-21 19:09:34 +0800

    fix

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -10,7 +10,7 @@ export default [
     routes: [
       {
         path: '/',
-        wrappers: ['@/layouts/dynamic'],
+        wrappers: ['@/layouts/hackthon03'],
         component: '@/pages/home',
         title: 'Home',
         exact: true,
